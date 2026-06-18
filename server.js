79d4ab7fefb47d0af580acacb99f1043e39e8ea2
damaris-tah <damaristah4@gmail.com>
2026-06-18 08:04:05 -0900

    Register authentication routes in server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const connectDB = require('./config/db'); // import db connection
+const connectDB = require('./config/db'); 
+const authRoutes = require('./routes/auth'); 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Routes
+app.use('/api/auth', authRoutes); 
 
 // Test route
 app.get('/', (req, res) => res.send('PropSpace API running...'));
