acead0fe23f52d98e1ec14b28f3d63d8e9dbc9c9
damaris-tah <damaristah4@gmail.com>
2026-06-18 08:24:25 -0900

    Add property CRUD routes and Property model

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db'); 
 const authRoutes = require('./routes/auth'); 
+const propertyRoutes = require('./routes/property'); 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use('/api/auth', authRoutes); 
+app.use('/api/auth', authRoutes);       
+app.use('/api/properties', propertyRoutes); 
 
 // Test route
 app.get('/', (req, res) => res.send('PropSpace API running...'));
